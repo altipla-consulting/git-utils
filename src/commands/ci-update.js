1558b54d23112cd0c225a572c3ba75c06d86192b
Ernesto Alejo <ernestokarim@gmail.com>
2021-04-02 23:05:06 +0200

    Translate the warning message for ci update.

--- src/commands/ci-update.js
+++ src/commands/ci-update.js
@@ -8,8 +8,8 @@ const { ensureOpen, showText } = require('../output')
 module.exports = async function () {
   let changes = await sh(['git', 'status', '-s'])
   if (changes.length) {
-    let result = await window.showWarningMessage('El proyecto tiene cambios. ¿Estás seguro de que deseas borrar todo y actualizar?', 'Borrar y actualizar todo', 'Cancelar')
-    if (!result || result === 'Cancelar') {
+    let result = await window.showWarningMessage('There are local changes that will be LOST. Are you sure you want to proceed?', 'Delete and update everything', 'Cancel')
+    if (!result || result === 'Cancel') {
       return
     }
   }
