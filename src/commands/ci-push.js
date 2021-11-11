0ca4f638ada353c3683a859733dd2ff1855f456d
Ernesto Alejo <ernestokarim@gmail.com>
2021-11-12 00:48:23 +0100

    Detect workspaces without folders to warn about them before the erorr.

--- src/commands/ci-push.js
+++ src/commands/ci-push.js
@@ -1,9 +1,16 @@
 
+const { window, workspace } = require('vscode')
+
 const { shout } = require('../exec')
 const { ensureOpen, showText } = require('../output')
 
 
 module.exports = async function () {
+  if (!workspace.workspaceFolders) {
+    window.showWarningMessage('Open a workspace to run this command.')
+    return
+  }
+
   ensureOpen()
   showText('$ ci push\n')
 
--- src/commands/ci-update.js
+++ src/commands/ci-update.js
@@ -1,11 +1,16 @@
 
-const { window } = require('vscode')
+const { window, workspace } = require('vscode')
 
 const { shout, sh } = require('../exec')
 const { ensureOpen, showText } = require('../output')
 
 
 module.exports = async function () {
+  if (!workspace.workspaceFolders) {
+    window.showWarningMessage('Open a workspace to run this command.')
+    return
+  }
+
   let changes = await sh(['git', 'status', '-s'])
   if (changes.length) {
     let result = await window.showWarningMessage('There are local changes that will be LOST. Are you sure you want to proceed?', 'Delete and update everything', 'Cancel')
