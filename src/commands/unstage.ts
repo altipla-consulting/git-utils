c4a5dff04c6fa2904c92ecf31aca6c37b70b3456
Ernesto Alejo <ernestokarim@gmail.com>
2022-07-01 17:55:02 +0200

    Show error message if there is not an active text editor when unstaging.

--- src/commands/unstage.ts
+++ src/commands/unstage.ts
@@ -5,5 +5,10 @@ import { git } from '../exec'
 
 
 export async function cmdUnstage() {
+  if (!window.activeTextEditor) {
+    window.showWarningMessage('There is no active text editor.')
+    return
+  }
+  
   await git(['git', 'restore', '--staged', window.activeTextEditor.document.fileName])
 }
